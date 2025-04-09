106f421e04b40962292b9c47994f805abc9c06f9
shreyas801 <shreyastotagi143@gmail.com>
2025-04-09 15:01:20 +0530

    save

--- sub.java
+++ sub.java
@@ -5,7 +5,9 @@ class Sub
         int a=10;
         int b=20;
         int sub = a-b;
+        int add = a+b;
         System.out.println("sub is :"+sub);
+        System.out.println("add is"+add);
         
     }
 }
