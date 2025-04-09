9819fcd165fcb136ef8927830affc6a4bfa4d5a5
shreyas801 <shreyastotagi143@gmail.com>
2025-04-09 14:59:17 +0530

    save

---
+++ sub.java
@@ -0,0 +1,11 @@
+class Sub
+{
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=20;
+        int sub = a-b;
+        System.out.println("sub is :"+sub);
+        
+    }
+}
